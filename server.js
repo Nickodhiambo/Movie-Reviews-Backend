f023bc62e9dde2f1993115b0fdc43c8c69354341
Nick <nodhiambo01@gmail.com>
2024-04-06 12:41:21 +0300

    Test post review

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 // Express routes
-app.use('api/v1/reviews', reviews); // Enable app to use our API and routes defined within
+app.use('/api/v1/reviews', reviews); // Enable app to use our API and routes defined within
 
 // Handle all other routes not included in our API
 app.use('*', (req, res) => {
-    res.status(404).json({'error': 'Not found'})
+    res.status(404).json({'error': 'Not Found'})
 });
 
 export default app; // Export our code to be used in a file that accesses the database import
